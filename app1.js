7a4900acf37c1e282244f27dc43cb7b0302acbe8
Gian Franco Crova <103906625+gfCrova@users.noreply.github.com>
2022-10-17 16:01:56 -0300

    Update app1.js

--- app1.js
+++ app1.js
@@ -1,13 +1,9 @@
 
 
-//Este codigo indica que una vez cargue la página, ejecuta ese string en la consola//
 window.addEventListener("load", function() {
     console.log("El contenido a cargado!");
 
-
-//Este codigo crea un slider de 3 imagenes que cambian cada 2 segundos//
-    
-   //se crea una variable de un Array//
+   
     var imagen=[];
 
         imagen[0]= "img/imagen1.jpg"
@@ -19,8 +15,6 @@ window.addEventListener("load", function() {
         function cambiarImagenes() {
 
             document.slider.src= imagen[indiceImagenes];
-
-//La condición dice si (if) la VAR indiceIm es menor a 2 se incrementa en 1, pero (else) si ya llego a su límite (2) entonces vuelve a su inico (0)// 
 
             if (indiceImagenes < 2) { indiceImagenes++;
             } else {
